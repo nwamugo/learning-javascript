8262cb21015fcb02a7a65fdaaf9c5b3dca23aa6e
Duziem Ugoji <tickettoduzz@gmail.com>
2024-03-07 08:39:24 +0100

    defaultfunctionargz edits

--- defaultfunctionargz.js
+++ defaultfunctionargz.js
@@ -10,43 +10,42 @@ makeAjaxRequest('google.com', 'GET');
 makeAjaxRequest('google.com', 'POST');
 
 
-
-
-//refactor
-//to make use of default function arguments, instead of the tedious
-//check we did with the if statement above
+// refactor
+// to make use of default function arguments, instead of the tedious
+// check we did with the if statement above
 
 function makeAjaxRequest(url, method = 'GET') {
     //logic to make the request
 }
 
-
 //if you want the method to deliberately be empty, then
-makeAjaxRequest('google.com', null); //method will not be reassigned to default
+makeAjaxRequest('google.com', null); // method will not be reassigned to default
 
 
 //if you use undefined instead of null, method will yes be reassigned to default
 makeAjaxRequest('google.com', undefined);
 
+// With default parameters, 0 or "" or null are valid values and will not
+// be replaced by the default value.
 
+// Below is a constructor function
 function User(id) {
     this.id = id;
 }
 
-//we want to generate random id
+// generate random id
 function generateId() {
     return Math.random() * 999999;
 }
 
-function createAdminUser() {
+function createAdminUser(user) {
     user.admin = true;
     return user;
 }
 
-
 createAdminUser(new User(generateId()));
 
-//refactor
+// refactor to accomodate whether a user parameter is passed in or not
 function createAdminUser(user = new User(generateId())) {
     user.admin = true;
     return user;
@@ -57,3 +56,13 @@ createAdminUser();
 //If I have a user already, I could pass in the user as well
 const user = new User(generateId());
 createAdminUser(user);
+
+
+function getPonies(size = defaultSize(), page = size - 1) {
+    server.get(size, page)
+}
+
+
+const { timeout = 1000 } = httpOptions;
+// timeout will be the value of httpOptions.timeout if it exists
+// or 1000 if not
